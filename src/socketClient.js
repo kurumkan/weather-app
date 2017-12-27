42ebc5024a2704e9b250e5657654b1b2d467e3ba
Artur Arsalanov <joomrise@gmail.com>
2017-12-27 21:25:02 +0300

    add Procfile for heroku

--- src/socketClient.js
+++ src/socketClient.js
@@ -2,7 +2,8 @@ import { store } from 'index';
 import io from 'socket.io-client';
 import { setActiveClients, notifyClientStatusChange } from 'actions/usersActions';
 
-const socket = io('http://localhost:5000');
+const API_URL = process.env.NODE_ENV === 'production' ? window.location.host : 'http://localhost:5000';
+const socket = io(API_URL);
 
 socket.on('SET_ACTIVE_CLIENTS', data => {
   store.dispatch(setActiveClients(Object.values(data)));
