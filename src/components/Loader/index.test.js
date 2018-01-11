b47d78d6934b691c4a5c7f6d70db1964ce5a8d74
Artur Arsalanov <joomrise@gmail.com>
2018-01-11 03:27:38 +0300

    config jest

--- src/components/Loader/index.test.js
+++ src/components/Loader/index.test.js
@@ -1,5 +1,12 @@
-describe('Addition', () => {
-  it('knows that 2 and 2 make 4', () => {
-    expect(2 + 2).toBe(4);
+import Loader from './index.js';
+import React from 'react';
+import { mount } from 'enzyme';
+
+describe('Loader', () => {
+  it('renders', () => {
+    const wrapper = mount(
+      <Loader />
+    );
+    expect(wrapper.find('.loader-wrapper').exists()).toEqual(true);
   });
 });
